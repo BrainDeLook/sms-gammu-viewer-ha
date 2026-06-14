08566dd90d882ccd09e45d163e0e250e950e065d
BrainDeLook <148875214+BrainDeLook@users.noreply.github.com>
2026-06-14 21:48:20 +0300

    Add constants

--- custom_components/sms_gammu_viewer/const.py
+++ custom_components/sms_gammu_viewer/const.py
@@ -1,17 +1,17 @@
 """Constants for SMS Gammu Viewer."""
 
 DOMAIN = "sms_gammu_viewer"
+
 PANEL_URL = "sms-viewer"
 PANEL_TITLE = "SMS"
 PANEL_ICON = "mdi:message-text"
-FRONTEND_PATH = "ha_frontend"
+FRONTEND_PATH = "frontend"
 
 CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 
-DEFAULT_HOST = "localhost"
 DEFAULT_PORT = 5000
 DEFAULT_USERNAME = "admin"
 DEFAULT_PASSWORD = "password"
