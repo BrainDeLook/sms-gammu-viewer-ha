b81e635cef60f3c1f4d5fda6cb92dd681efa8036
BrainDeLook <148875214+BrainDeLook@users.noreply.github.com>
2026-06-15 02:50:20 +0300

    v2: constants

---
+++ custom_components/sms_gammu_viewer/const.py
@@ -0,0 +1,22 @@
+"""Constants."""
+
+DOMAIN = "sms_gammu_viewer"
+
+PANEL_URL = "sms-viewer"
+PANEL_TITLE = "SMS"
+PANEL_ICON = "mdi:message-text"
+FRONTEND_PATH = "frontend"
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_POLL_INTERVAL = "poll_interval"
+CONF_NOTIFY_TARGETS = "notify_targets"
+
+DEFAULT_PORT = 5000
+DEFAULT_USERNAME = "admin"
+DEFAULT_PASSWORD = "password"
+DEFAULT_POLL_INTERVAL = 30
+
+DB_FILENAME = "sms_gammu_viewer.db"
