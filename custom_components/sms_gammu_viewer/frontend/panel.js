7b6fba9df202c7c33f04cb0e52cf632d4c6116b7
BrainDeLook <148875214+BrainDeLook@users.noreply.github.com>
2026-06-27 16:22:14 +0300

    Messages area: fade gradient at bottom

--- custom_components/sms_gammu_viewer/frontend/panel.js
+++ custom_components/sms_gammu_viewer/frontend/panel.js
@@ -325,6 +325,9 @@ const CSS = `
     display: flex;
     flex-direction: column;
     gap: 10px;
+  
+    -webkit-mask-image: linear-gradient(to bottom, black calc(100% - 80px), transparent 100%);
+    mask-image: linear-gradient(to bottom, black calc(100% - 80px), transparent 100%);
   }
 
   .msg-ctx-menu {
